98bba52b602f89c85698288a6775bf1f9a6c2062
altair <aqibulhasan72@gmail.com>
2020-12-06 21:52:09 +0600

    BookList APP updated

--- bookListApp/app.js
+++ bookListApp/app.js
@@ -1,45 +1,131 @@
-const form = document.querySelector("form");
-const tableBody = document.querySelector("tbody");
-const input = document.querySelectorAll("input");
-const p = document.querySelector("p");
-
-function alert(str) {
-  const span = document.createElement("span");
-  span.innerHTML = `Book ${str}`;
-  if (str === "Added") {
-    span.classList.add("green");
-  } else if (str === "Deleted") {
-    span.classList.add("red");
+//Book class
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
   }
-
-  if ((p.innerHTML = " ")) {
-    p.append(span);
-  }
-  setTimeout(() => {
-    span.remove();
-  }, 2000);
 }
 
-form.addEventListener("submit", (e) => {
+//Store class
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+    return books; //array of object
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+    books.forEach((book, index) => {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+    //resetting the 'books' in the localstorage
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+}
+
+//UI class
+class UI {
+  static displayBooks() {
+    const books = Store.getBooks();
+    console.log(books);
+
+    for (let book of books) {
+      UI.addBookToList(book);
+    }
+  }
+  static addBookToList(book) {
+    const list = document.querySelector("#book-list");
+    const row = document.createElement("tr");
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+    `;
+
+    list.append(row);
+  }
+
+  //creating alert
+  static showAlert(message, className) {
+    const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+    // div.appendChild(document.createTextNode(message));
+    div.innerHTML = `${message}`;
+    const container = document.querySelector(".container");
+    const h1 = document.querySelector("h1");
+    const form = document.querySelector("#book-form");
+    container.insertBefore(div, form);
+
+    //removing alert
+    setTimeout(function () {
+      div.remove();
+    }, 3000);
+  }
+
+  static clearFields() {
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#isbn").value = "";
+  }
+  static deleteBook(target) {
+    if (target.classList.contains("delete")) {
+      target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+//Event:Display Books
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
+
+//Event:Add a Book
+document.querySelector("#book-form").addEventListener("submit", (e) => {
   e.preventDefault();
-  const row = document.createElement("tr");
-  const button = document.createElement("button");
-  button.innerHTML = `<i class="fas fa-times"></i>`;
-  for (let i = 0; i < input.length; i++) {
-    const column = document.createElement("td");
+  //form value
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const isbn = document.querySelector("#isbn").value;
 
-    column.innerHTML = input[i].value;
-    row.append(column);
+  //validating
+  if (title === "" || author === "" || isbn === "") {
+    UI.showAlert("Please fill in all details", "danger");
+  } else {
+    //instantiating books
+    const book = new Book(title, author, isbn);
+
+    //Add book to localstorage
+    Store.addBook(book);
+
+    //add book to list
+    UI.addBookToList(book);
+
+    //showing success alert
+    UI.showAlert("Book Added", "success");
+
+    //clear input fields
+    UI.clearFields();
   }
-  row.append(button);
-  tableBody.append(row);
-  alert("Added");
 });
+//Event:Delete a Book
+document.querySelector("#book-list").addEventListener("click", (e) => {
+  UI.deleteBook(e.target);
 
-// Delete book
-tableBody.addEventListener("click", function (e) {
-  if (e.target.className === "fas fa-times") {
-    e.target.parentElement.parentElement.remove();
-    alert("Deleted");
-  }
+  //delete from storage
+  Store.removeBook(e.target.parentElement.previousElementSibling.innerText);
+  //showing delete alert
+  UI.showAlert("Book Deleted", "success");
 });
